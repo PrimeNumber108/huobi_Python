b97c95e66a141c9455b75f82892a51ff165f3f92
dnkhiem <dnkhiem@gmail.com>
2024-10-17 16:32:39 -0400

    upgrade huobi

--- huobi/connection/impl/websocket_watchdog.py
+++ huobi/connection/impl/websocket_watchdog.py
@@ -1,75 +1,89 @@
 import threading
 import logging
-from apscheduler.schedulers.blocking import BlockingScheduler
-
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.executors.pool import ThreadPoolExecutor
 from huobi.connection.impl.private_def import *
 from huobi.utils.time_service import get_current_timestamp
 
 
 def watch_dog_job(*args):
+    """Kiểm tra các kết nối và quản lý trạng thái WebSocket."""
     watch_dog_obj = args[0]
 
     for idx, websocket_manage in enumerate(watch_dog_obj.websocket_manage_list):
-        if websocket_manage.request.auto_close:  # setting auto close no need reconnect
-            pass
-        elif websocket_manage.state == ConnectionState.CONNECTED:
-            if watch_dog_obj.is_auto_connect:
-                ts = get_current_timestamp() - websocket_manage.last_receive_time
-                if ts > watch_dog_obj.heart_beat_limit_ms:
-                    watch_dog_obj.logger.warning("[Sub][" + str(websocket_manage.id) + "] No response from server")
-                    websocket_manage.close_and_wait_reconnect(watch_dog_obj.wait_reconnect_millisecond())
+        if websocket_manage.request.auto_close:
+            continue  # Bỏ qua nếu auto_close được bật
+
+        ts = get_current_timestamp() - websocket_manage.last_receive_time
+        if websocket_manage.state == ConnectionState.CONNECTED:
+            if watch_dog_obj.is_auto_connect and ts > watch_dog_obj.heart_beat_limit_ms:
+                watch_dog_obj.logger.warning(f"[Sub][{websocket_manage.id}] No response from server")
+                websocket_manage.close_and_wait_reconnect(watch_dog_obj.wait_reconnect_millisecond())
+
         elif websocket_manage.state == ConnectionState.WAIT_RECONNECT:
             watch_dog_obj.logger.warning("[Sub] call re_connect")
             websocket_manage.re_connect()
-            pass
+
         elif websocket_manage.state == ConnectionState.CLOSED_ON_ERROR:
             if watch_dog_obj.is_auto_connect:
                 websocket_manage.close_and_wait_reconnect(watch_dog_obj.reconnect_after_ms)
-                pass
 
 
 class WebSocketWatchDog(threading.Thread):
+    """Quản lý WebSocket và duy trì kết nối."""
     mutex = threading.Lock()
-    websocket_manage_list = list()
+    websocket_manage_list = []
 
-    def __init__(self, is_auto_connect=True, heart_beat_limit_ms=CONNECT_HEART_BEAT_LIMIT_MS, reconnect_after_ms=RECONNECT_AFTER_TIME_MS):
+    def __init__(self, is_auto_connect=True, heart_beat_limit_ms=30000, reconnect_after_ms=60000):
         threading.Thread.__init__(self)
         self.is_auto_connect = is_auto_connect
         self.heart_beat_limit_ms = heart_beat_limit_ms
-        self.reconnect_after_ms = reconnect_after_ms if reconnect_after_ms > heart_beat_limit_ms else heart_beat_limit_ms
-        self.logger = logging.getLogger("huobi-client")
-        self.scheduler = BlockingScheduler()
+        self.reconnect_after_ms = max(reconnect_after_ms, heart_beat_limit_ms)
+
+        # Khởi tạo logger
+        self.logger = self._setup_logger()
+
+        # Khởi tạo scheduler với ThreadPoolExecutor
+        executors = {'default': ThreadPoolExecutor(max_workers=20)}
+        self.scheduler = BackgroundScheduler(executors=executors)
         self.scheduler.add_job(watch_dog_job, "interval", max_instances=10, seconds=10, args=[self])
         self.start()
 
+    def _setup_logger(self):
+        """Thiết lập logger."""
+        logger = logging.getLogger("huobi-client")
+        if not logger.hasHandlers():
+            handler = logging.StreamHandler()
+            handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+            logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+        return logger
+
     def run(self):
+        """Chạy scheduler trong thread."""
         try:
             self.scheduler.start()
         except (KeyboardInterrupt, SystemExit):
-            self.logger.info("Shutting down scheduler...")
-            self.scheduler.shutdown(wait=False)
+            self.stop()
+
     def stop(self):
+        """Dừng scheduler và thread an toàn."""
         self.logger.info("Stopping WebSocketWatchDog...")
-        self.scheduler.shutdown(wait=False)  # Dừng scheduler mà không chờ job hoàn thành
+        self.scheduler.shutdown(wait=False)  # Dừng scheduler ngay lập tức
         self.join()  # Chờ thread kết thúc
 
     def on_connection_created(self, websocket_manage):
-        self.mutex.acquire()
-        self.websocket_manage_list.append(websocket_manage)
-        self.mutex.release()
+        """Thêm kết nối WebSocket mới."""
+        with self.mutex:
+            self.websocket_manage_list.append(websocket_manage)
 
     def on_connection_closed(self, websocket_manage):
-        self.mutex.acquire()
-        self.websocket_manage_list.remove(websocket_manage)
-        self.mutex.release()
+        """Xóa kết nối WebSocket đã đóng."""
+        with self.mutex:
+            self.websocket_manage_list.remove(websocket_manage)
 
-    # calculate next reconnect time
     def wait_reconnect_millisecond(self):
-        wait_millisecond = int(self.reconnect_after_ms - self.heart_beat_limit_ms)
+        """Tính thời gian chờ trước khi thử lại kết nối."""
+        wait_millisecond = max(self.reconnect_after_ms - self.heart_beat_limit_ms, 1000)
         now_ms = get_current_timestamp()
-        wait_millisecond = wait_millisecond if wait_millisecond else 1000
-        # job loop after 1 second
-        return (wait_millisecond + now_ms)
-
-
-
+        return wait_millisecond + now_ms
